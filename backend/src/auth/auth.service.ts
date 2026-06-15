f6b7ff44de0a96ea18ecc0713cd03b8ecaa81b78
Aysha <aysha@MacBook-Air-Aysha.local>
2026-06-15 15:37:27 +0300

    ...

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async register(email: string, password: string) {
+    const existingUser = await this.usersService.findByEmail(email);
+
+    if (existingUser) {
+      throw new BadRequestException('User already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.usersService.create(email, hashedPassword);
+
+    return {
+      id: user.id,
+      email: user.email,
+    };
+  }
+}
